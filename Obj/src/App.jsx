7217dfb26a9d30b904215a6969b74d8722a94dc5
bantupallipavithra <bantupalli302@gmail.com>
2025-10-28 16:09:31 +0530

    objects

---
+++ Obj/src/App.jsx
@@ -0,0 +1,10 @@
+import Data from './Components/Data'
+function App(){
+  return(
+    <>
+   <h1>Welcome</h1>
+   <Data />
+    </>
+  )
+}
+export default App;
---
+++ Obj/src/Components/Data.jsx
@@ -0,0 +1,15 @@
+import Data1 from "../assets/Datajson.json"
+export default function Data(){
+    return(
+        <>
+        {
+            Data1.products.map((item,index)=>(
+                <div key={index}>
+                    <p>The name of the product is {item.name}</p>
+                    <p>The price of the product is {item.price}</p>
+                </div>
+            ))
+        }
+        </>
+    )
+}
